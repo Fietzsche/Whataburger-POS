3da7b01f34b8183327dd10710e32fa08f2c2c323
Jared <realjaredeveritt@gmail.com>
2019-03-15 21:50:20 -0500

    Added Order List text field and shutdown button

--- src/pos/Window.java
+++ src/pos/Window.java
@@ -8,8 +8,11 @@ public class Window extends JFrame {
 	
 	/* Startup Screen */
 	private JButton startPOS;
+	private JButton shutdown;
 	
 	/* Order Screen */
+	private JTextField orderList;
+	
 	private JButton whataburger;
 	private JButton dblWhataburger;
 	private JButton trplWhataburger;
@@ -20,6 +23,9 @@ public class Window extends JFrame {
 		startPOS = new JButton("Start POS");
 		add(startPOS);
 		
+		shutdown = new JButton("Shutdown");
+		add(shutdown);
+		
 		listener buttonListener = new listener();
 		startPOS.addActionListener(buttonListener);
 	}
@@ -28,24 +34,38 @@ public class Window extends JFrame {
 		
 		public void actionPerformed(ActionEvent event) {
 			
-			if(event.getSource() == startPOS) {
+			if(event.getSource() == startPOS || event.getSource() == whataburger) {
 				remove(startPOS);
+				remove(shutdown);
 				revalidate();
 				repaint();
 				setUpOrderScreen();
+			}
+			
+			if(event.getSource() == shutdown) {
+				orderList.setText(whataburger.getText());
+				System.out.println(whataburger.getText());
 			}
 			
 		}
 	}
 		
 	public void setUpOrderScreen() {
+		
+		JTextField orderList = new JTextField("test");
 			
 		JButton whataburger = new JButton("Whataburger");
 		JButton dblWhataburger = new JButton("Double Meat Whataburger");
 		JButton trplWhataburger = new JButton("Triple Meat Whataburger");
-			
+		
+		add (orderList);
+		
 		add(whataburger);
 		add(dblWhataburger);
 		add(trplWhataburger);
+		
+		listener orderListener = new listener();
+		whataburger.addActionListener(orderListener);
+		
 	}
 }
